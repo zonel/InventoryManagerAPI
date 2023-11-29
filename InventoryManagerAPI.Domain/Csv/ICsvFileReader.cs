b0ffc5ddcd63fcf22911c47108fa06b110c565a8
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-29 20:22:31 +0100

    [Add] CsvFileReader

---
+++ InventoryManagerAPI.Domain/Csv/ICsvFileReader.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagerAPI.Domain.Csv;
+
+public interface ICsvFileReader
+{
+    public Task<IEnumerable<string>> ReadFileAsync(string filePath);
+}
---
+++ InventoryManagerAPI.Infrastructure/Csv/Read.cs
@@ -0,0 +1,35 @@
+﻿using InventoryManagerAPI.Domain.Csv;
+
+namespace InventoryManagerAPI.Infrastructure.Csv;
+
+public class Read : ICsvFileReader
+{
+    public async Task<IEnumerable<string>> ReadFileAsync(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            throw new FileNotFoundException($"File '{filePath}' not found.");
+        }
+
+        var lines = new List<string>();
+
+        try
+        {
+            using (var reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = await reader.ReadLineAsync()) != null)
+                {
+                    lines.Add(line);
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error reading file: {ex.Message}");
+            throw;
+        }
+
+        return lines;
+    } 
+}
