688092f7629873fc106fe68610302f1a968de529
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-30 13:31:27 +0100

    [Add] DefaultTypeConverter

---
+++ InventoryManager.Application/Mapping/TypeConverters/NullableIntConverter.cs
@@ -0,0 +1,26 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace InventoryManager.Application.Mapping.TypeConverters;
+
+public class NullableIntConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            // Return null for empty or whitespace strings
+            return null;
+        }
+
+        if (int.TryParse(text, out int result))
+        {
+            // If conversion to int succeeds, return the value
+            return result;
+        }
+
+        // Return null if conversion fails
+        return null;
+    }
+}
---
+++ InventoryManager.Application/Mapping/TypeConverters/PercentageNullableDecimalConverter.cs
@@ -0,0 +1,25 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace InventoryManager.Application.Mapping.TypeConverters;
+
+public class PercentageNullableDecimalConverter : DefaultTypeConverter
+{
+    public override object ConvertFromString(string text, IReaderRow row, MemberMapData memberMapData)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+        {
+            return null;
+        }
+
+        if (decimal.TryParse(text.TrimEnd('%'), out decimal result))
+        {
+            // Divide by 100 to convert percentage to a decimal value
+            return result / 100;
+        }
+
+        // Return null if conversion fails
+        return null;
+    }
+}
