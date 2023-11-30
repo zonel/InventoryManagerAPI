0ba5bed380416bcc1655a49b34a5fb47a5311033
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-30 13:24:49 +0100

    [Add] CsvMapper

---
+++ InventoryManager.Application/Mapping/MapCsv.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using CsvHelper;
+using CsvHelper.Configuration;
+using InventoryManager.Application.Mapping.EntityMappings;
+using InventoryManagerAPI.Domain.Mapping;
+using InventoryManagerAPI.Domain.Models;
+
+namespace InventoryManager.Application.Mapping;
+
+public class MapCsv : ICsvMapper
+{
+    private readonly IMappingConfigurationsFactory _mappingConfigurationsFactory;
+
+    public MapCsv(IMappingConfigurationsFactory mappingConfigurationsFactory)
+    {
+        _mappingConfigurationsFactory = mappingConfigurationsFactory;
+    }
+    public async Task<List<T>> MapCsvAsync<T>(List<string> csvLines, CsvConfiguration config, ClassMap<T> classMap)
+    {
+        try
+        {
+            using (var reader = new StringReader(string.Join(Environment.NewLine, csvLines)))
+            using (var csv = new CsvReader(reader, config))
+            {
+                csv.Context.RegisterClassMap(classMap.GetType()); 
+                var records = csv.GetRecords<T>().ToList();
+                
+                return records;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error mapping CSV: {ex.Message}");
+            throw;
+        }
+    }
+}
---
+++ InventoryManagerAPI.Domain/Mapping/ICsvMapper.cs
@@ -0,0 +1,8 @@
+﻿using CsvHelper.Configuration;
+
+namespace InventoryManagerAPI.Domain.Mapping;
+
+public interface ICsvMapper
+{
+    public Task<List<T>> MapCsvAsync<T>(List<string> csvLines, CsvConfiguration config, ClassMap<T> classMap);
+}
