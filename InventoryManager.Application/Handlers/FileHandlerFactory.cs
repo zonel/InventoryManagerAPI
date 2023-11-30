21792dd421a2908412a7e37cccb4a8c3a1c3d49f
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-30 13:12:04 +0100

    [Add] FileHandlerFactory

---
+++ InventoryManager.Application/Handlers/FileHandlerFactory.cs
@@ -0,0 +1,34 @@
+﻿using InventoryManagerAPI.Domain.Handler;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InventoryManager.Application.Handlers;
+
+public class FileHandlerFactory : IFileHandlerFactory
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public FileHandlerFactory(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+
+    public IFileHandler GetFileHandler(string filePath)
+    {
+        if (filePath.Contains("Inventory.csv"))
+        {
+            return _serviceProvider.GetRequiredService<InventoryFileHandler>();
+        }
+        else if (filePath.Contains("Products.csv"))
+        {
+            return _serviceProvider.GetRequiredService<ProductFileHandler>();
+        }
+        else if (filePath.Contains("Prices.csv"))
+        {
+            return _serviceProvider.GetRequiredService<PriceFileHandler>();
+        }
+        else
+        {
+            throw new NotSupportedException($"No handler found for file: {filePath}");
+        }
+    }
+}
---
+++ InventoryManagerAPI.Domain/Handler/IFileHandlerFactory.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagerAPI.Domain.Handler;
+
+public interface IFileHandlerFactory
+{
+    public IFileHandler GetFileHandler(string filePath);
+}
