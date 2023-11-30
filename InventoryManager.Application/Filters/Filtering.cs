86fac29b5c95e789daa965349b4c7a77bd56ab41
zonel <bartlomiej.bobowski@microsoft.wsei.edu.pl>
2023-11-30 13:30:15 +0100

    [Add] Filtering

---
+++ InventoryManager.Application/Filters/Filtering.cs
@@ -0,0 +1,11 @@
+﻿using InventoryManagerAPI.Domain.Filtering;
+
+namespace InventoryManager.Application.Filters;
+
+public class Filtering : IFilterEntity
+{
+    public List<T> FilterEntities<T>(List<T> entities, Func<T, bool> predicate)
+    {
+        return entities.Where(predicate).ToList();
+    }
+}
---
+++ InventoryManagerAPI.Domain/Filtering/IFilterEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace InventoryManagerAPI.Domain.Filtering;
+
+public interface IFilterEntity
+{
+    public List<T> FilterEntities<T>(List<T> entities, Func<T, bool> predicate);
+}
